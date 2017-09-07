9e039c4a4427d91bed8f2697b934e690d6476d43
benez655 <31712050+benez655@users.noreply.github.com>
2017-09-07 14:31:18 -0400

    Russian Peasant Multiplication
    First Code

---
+++ RusssianPeasant.py
@@ -0,0 +1,22 @@
+'''
+Benjamin Day
+8/30/2017
+Russian Peasant Mutiplication
+'''
+
+#input two multiplicands into 'A' and 'B', 'mySum' is the total
+A=int(input('First Multiplicand = '))
+B=int(input('Second Multiplicand =  '))
+mySum=0
+
+#while A is larger than one divide it by 2 (removing remainders)
+while A>1:
+    A=A//2
+    #check to see if the value in A is even by seeing if there is a remainder after dividing by 2
+    #if theres is no remainder, double B, else double B and add it to the total 'mySum'
+    if (A%2)==0:
+        B=B*2
+    else:
+        B=B*2
+        mySum=B+mySum
+print(mySum)
