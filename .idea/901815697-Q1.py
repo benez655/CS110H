4fe044c066f9f3cb6972535fb5b2582061ae9f7b
benez655 <31712050+benez655@users.noreply.github.com>
2017-09-18 15:44:09 -0400

    update

--- .idea/901815697-Q1.py
+++ .idea/901815697-Q1.py
@@ -4,13 +4,17 @@ Benjamin Day
 Quiz 1
 '''
 
+#input 3 values and assign them to 'A','B', and 'C'
 A=float(input('First Number: '))
 B=float(input('Second Number: '))
 C=float(input('Third Number: '))
 
+#check if the numbers are in increasing order and if so print 'the numbers are in increasing order'
 if A<B<C:
     print('the numbers are in increasing order')
+#check if the numbers are in decreasing order and if so print 'the numbers are in decreasing order'
 elif A>B>C:
     print('the numbers are in decreasing order')
+#otherwise print that the numbers are random
 else:
     print('the numbers are in a random order')
