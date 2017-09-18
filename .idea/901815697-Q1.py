cf6169ca0dce72d7c01d8dcd4bcc8b75e70258d1
benez655 <31712050+benez655@users.noreply.github.com>
2017-09-18 15:58:35 -0400

    update

--- .idea/901815697-Q1.py
+++ .idea/901815697-Q1.py
@@ -1,7 +1,11 @@
 '''
 Benjamin Day
 9/18/2017
-Quiz 1 q1
+Quiz 1
+'''
+
+'''
+Q1
 '''
 
 #input 3 values and assign them to 'A','B', and 'C'
@@ -18,3 +22,41 @@ elif A>B>C:
 #otherwise print that the numbers are random
 else:
     print('the numbers are in a random order')
+
+
+
+
+'''
+Q2
+'''
+
+#assign a desired value to 'A'
+A=int(input('Give me a number between 1 and 12: '))
+
+if A>=1 and A<=12:                                  #check to see if the input value is between 1 and 12
+    if A==1:
+        print('That is January')
+    elif A==2:
+        print('That is February')
+    elif A==3:
+        print('That is March')
+    elif A==4:
+        print('That is April')
+    elif A==5:
+        print('That is May')
+    elif A==6:                                        #checks the value of 'A' and prints corresponding month
+        print('That is June')
+    elif A==7:
+        print('That is July')
+    elif A==8:
+        print('That is August')
+    elif A==9:
+        print('That is September')
+    elif A==10:
+        print('That is October')
+    elif A==11:
+        print('That is November')
+    elif A==12:
+        print('That is December')
+else:                                     #if the number is outtside the range between 1 nad 12 then prints a response to the user
+    print('I said BETWEEN 1 and 12!')
